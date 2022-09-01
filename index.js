f31693ca64b3b1deef7d4265d9464dc0e2d1aa93
Marco <marco.salman@gmail.com>
2022-08-31 22:46:38 -0400

    adding player and keys

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ class Boundary {
   }
 }
 
+class Player {
+  constructor ({position, velocity}) {
+    this.position = position
+    this.velocity = velocity
+    this.radius = 15
+  }
+
+  draw() {
+    c.beginPath()
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+    c.fillStyle = 'yellow'
+    c.fill()
+    c.closePath()
+  }
+}
+
 const map = [
   ['-','-','-','-','-','-'],
   ['-',' ',' ',' ',' ','-'],
@@ -27,12 +43,23 @@ const map = [
   ['-','-','-','-','-','-'],
 ]
 const boundaries = []
- 
+const player = new Player({
+  position: {
+    x: Boundary.width + Boundary.width/2,
+    y: Boundary.height + Boundary.height/2
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  }
+})
+
 map.forEach((row, i) => {
   row.forEach((symbol, j) => {
     switch (symbol) {
       case '-': 
-      boundaries.push(new Boundary({
+      boundaries.push(
+        new Boundary({
         position: {
           x: Boundary.width * j,
           y: Boundary.height * i
@@ -46,4 +73,24 @@ map.forEach((row, i) => {
 
 boundaries.forEach((boundary) => {
   boundary.draw()
+})
+
+player.draw()
+
+window.addEventListener('keydown', ({key}) => {
+  console.log(key)
+  switch (key) {
+    case 'w':
+      player.velocity.y = -5
+    break
+    case 'a':
+      player.velocity.x = -5
+    break
+    case 's':
+      player.velocity.y = 5
+    break
+    case 'd':
+      player.velocity.x = 5
+    break
+  }
 })
