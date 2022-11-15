4497abe2ed33257f710ce95209682cce431c988a
Marco <marco.salman@gmail.com>
2022-11-14 21:27:55 -0500

    add powerup

--- index.js
+++ index.js
@@ -86,8 +86,24 @@ class Pellet {
   }
 }
 
+class PowerUp {
+  constructor({ position }) {
+    this.position = position;
+    this.radius = 10;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = "white";
+    c.fill();
+    c.closePath();
+  }
+}
+
 const pellets = [];
 const boundaries = [];
+const PowerUp = [];
 const ghosts = [
   new Ghost({
     position: {
